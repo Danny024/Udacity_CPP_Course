805e53de287fbf84c91056390c83371640a34d5c
Daniel Eneh <72442267+Danny024@users.noreply.github.com>
2023-12-23 04:35:59 +0000

    Update template.cpp

--- C++ Udacity/template.cpp
+++ C++ Udacity/template.cpp
@@ -17,10 +17,17 @@ T Product (T a, T b){
     return a * b;
 }
 
+template <typename T>
+T Max (T a, T b){
+    return a > b? a : b;
+}
 int main (){
     assert (Sum <double> (4,5)==9);
     assert (Product<int>(5,5)==25);
+    assert (Max<double>(50.00,200.00)==200.00);
     cout<<Sum <double> (4,5)<<endl;
     cout <<Product<int>(5,5)<<endl;
+    cout <<Max<double> (50.00, 200.00)<<endl;
+
     
 }
